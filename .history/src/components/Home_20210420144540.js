9206203f72f68ef2db39f2e94fce452b963d8bce
ahmadsaadkhan <ahmad.saad.khan@live.com>
2021-05-03 12:45:17 +0300

    gitignore updated

--- .history/src/components/Home_20210420144540.js
+++
@@ -1,9 +0,0 @@
-import useFetch from '../components/useFetch';
-
-const Home = () => {
-    const {data, isPending, isError} = useFetch();
-    console.log(data);
-    return (  )
-}
- 
-export default Home;
